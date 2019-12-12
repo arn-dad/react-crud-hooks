09193bfdff80350d472c9290dfe3e63e6e56e4c0
arnold <dadayanaa@yahoo.com>
2019-12-12 23:26:05 +0400

    done

--- src/App.js
+++ src/App.js
@@ -1,69 +1,26 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import Form from './components/form';
 import Card from './components/card';
-import { example, dummy } from './constant';
+import { createEvent, updateEvent, removeEvent } from './actions'
 import './redux/store/store'
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-function App() {
-  const [state, setState] = useState({ events: [ ...dummy ], template: { ...example } })
-  const { events, template } = state;
-  
-  const submit = (data) => {
-    if (data.id) {
-      const newEvents = events.map((event) => {
-        return event.id === data.id ? data : event
-      })
-      setState({
-        ...state,
-        events: [ ...newEvents ],
-        template: { ...example  }
-      })
-      return
-    }
-    const newEvent = {
-      ...data,
-      id: new Date().valueOf(),
-    }
-    setState({
-      ...state,
-      events: [ ...events, newEvent ],
-      template: { ...example }
-    })
-  }
-
-  const handleRemove = (id) => {
-    const filteredEvents = events.filter((event) => {
-      return event.id !== id
-    });
-    setState({
-      ...state,
-      events: filteredEvents
-    })
-  }
-
-  const handleUpdate = (id) => {
-    const findEvent = events.find((event) => {
-      return event.id === id
-    });
-    setState({
-      ...state,
-      template: findEvent
-    })
-  }
-  
+function App(props) {
+  const { events, template, createEvent, removeEvent, updateEvent } = props;
+ 
   return (
     <div className="App">
       <div className="form-container">
-        <Form template={template} submit={submit}/>  
+        <Form template={template} submit={createEvent}/>  
       </div>
 
       <div className="todos">
         {events.map((event) => {
           return (
-            <Card key={event.id} onRemove={handleRemove} onUpdate={handleUpdate} data={event}/>
+            <Card key={event.id} onRemove={removeEvent} onUpdate={updateEvent} data={event}/>
           )
         })}
       </div>      
@@ -71,4 +28,27 @@ function App() {
   );
 }
 
-export default App;
+const mapSateToProps = (state) => {
+  return state.events
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createEvent: (id) => {
+      dispatch(createEvent(id))
+    },
+    updateEvent: (id) => {
+      dispatch(updateEvent(id))
+    },
+    removeEvent: (id) => {
+      dispatch(removeEvent(id))
+    }
+  }
+}
+
+const connectedApp = connect(
+  mapSateToProps,
+  mapDispatchToProps,
+)(App);
+
+export default connectedApp;

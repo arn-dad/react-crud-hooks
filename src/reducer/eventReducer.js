ec7eac7a1991d9c268e86c51af5d39671243975a
arnold <dadayanaa@yahoo.com>
2019-12-12 22:59:19 +0400

    add reducer

---
+++ src/reducer/eventReducer.js
@@ -0,0 +1,46 @@
+import { example, dummy } from '../constant';
+
+const initialState = { events: [ ...dummy ], template: { ...example } }
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'CREATE':
+        if (action.payload.id) {
+          const newEvents = state.events.map((event) => {
+            return event.id === action.payload.id ? action.payload : event
+          })
+         return {
+            ...state,
+            events   : [ ...newEvents ],
+            template : { id: null, name: '', location: '',  description: '' }
+          }
+        }
+        const newEvent = {
+          ...action.payload,
+          id: new Date().valueOf(),
+        }
+        return {
+          ...state,
+          events   : [ ...state.events, newEvent ],
+          template : { id: null, name: '', location: '',  description: '' }
+        }
+    case 'REMOVE':
+        const filteredEvents = state.events.filter((event) => {
+          return event.id !== action.payload
+        });
+        return {
+          ...state,
+          events   : [ ...filteredEvents ]
+        }
+    case 'UPDATE': 
+        const findEvent = state.events.find((event) => {
+          return event.id === action.payload
+        }); 
+        return {
+          ...state,
+          template: findEvent
+        }
+    default:
+      return state
+  }
+}
